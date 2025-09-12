83af71d898123c96908d69b6e2c874b1b281e4a7
Wesley.Nascimento <wesley.nascimento@wittel.com>
2025-09-12 10:53:12 -0300

    teste pipeline 2

--- src/actions/invoke.ts
+++ src/actions/invoke.ts
@@ -6,7 +6,7 @@ import { Reservation } from "twilio-taskrouter/dist/types";
 import { LocalStorageItemType } from "../helpers/localStorage/LocalStorageItemType";
 
 const workerClient = userInstance.getWorkerClient();
-console.log('teste pipeline3')
+console.log('teste pipeline4')
 export const invokeAction = {
     navigateToView: async (viewName: string) => {
         await Actions.invokeAction('NavigateToView', { viewName })
