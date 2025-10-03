9b45bb1033ef07e3fa5148b8231d4cee3ef2e999
Wesley.Nascimento <wesley.nascimento@wittel.com>
2025-10-03 14:01:52 -0300

    teste pipeline 12

--- src/states/activitiesFlagColor/ActivitiesFlagColor.ts
+++ src/states/activitiesFlagColor/ActivitiesFlagColor.ts
@@ -1,14 +1,22 @@
 import { Reducer, AnyAction } from "redux";
-import { ActivityColorType } from "../../types/activities";
+import { ActivityColorType } from '../../types/activities';
 
 export const initialState: ActivityColorType = {};
 
-export const reducer: Reducer<ActivityColorType, AnyAction> = (
-  state = initialState,
-  action
-) => {
-  if (action.type === "SET_DATA_ACTIVITIES_FLAG_COLOR" && "payload" in action) {
-    return action.payload;
-  }
-  return state;
+export type ActionType = {
+  type: 'SET_DATA_ACTIVITIES_FLAG_COLOR',
+  payload: ActivityColorType;
+}
+
+export const reducer: Reducer<ActivityColorType, AnyAction> = (state = initialState, action: AnyAction) => {
+  switch(action.type) {
+    case 'SET_DATA_ACTIVITIES_FLAG_COLOR': {
+      if(action?.payload) {
+        return action.payload;
+      }
+      return state;
+    }
+    default:
+      return state;
+  }  
 };
--- src/states/activityTeamsSync/ActivityTeamsSync.ts
+++ src/states/activityTeamsSync/ActivityTeamsSync.ts
@@ -1,14 +1,22 @@
 import { Reducer, AnyAction } from "redux";
-import { TeamsActivitiesDataType } from "../../types/activities";
+import { TeamsActivitiesDataType } from '../../types/activities';
 
 export const initialState: TeamsActivitiesDataType[] = [];
 
-export const reducer: Reducer<TeamsActivitiesDataType[], AnyAction> = (
-  state = initialState,
-  action
-) => {
-  if (action.type === "SET_DATA_TEAMS_ACTIVITIES" && "payload" in action) {
-    return action.payload;
-  }
-  return state;
+export type ActionType = {
+  type: 'SET_DATA_TEAMS_ACTIVITIES',
+  payload: TeamsActivitiesDataType[]
+}
+
+export const reducer: Reducer<TeamsActivitiesDataType[], AnyAction> = (state = initialState, action: AnyAction) => {
+  switch(action.type) {
+    case 'SET_DATA_TEAMS_ACTIVITIES': {
+      if(action?.payload) {
+        return action.payload;
+      }
+      return state;
+    }
+    default:
+      return state;
+  }  
 };
